48b96ebf7d1456ea203aac488a854b51d472e0f6
--unset <--unset>
2024-01-10 16:49:16 +0100

    added bubble sort function

--- cppProject/randArr.cpp
+++ cppProject/randArr.cpp
@@ -11,16 +11,36 @@ void fillWithRand(int* arr, int len){
     };
 }
 
+void displayArray(int* arr, int len){
+    for(int i = 0; i < len; i++){
+        cout << *(arr + i) << "\t";
+    };
+    cout << endl;
+}
+
+void bubbleSort(int* arr, int len){
+    int temp;
+    for(int i = 0; i < len; i++){
+        for(int j = 1; j < len + 1; j++){
+            if(*(arr + (j - 1)) > *(arr + j)){
+                temp = *(arr + j);
+                *(arr + j) = *(arr + j - 1);
+                *(arr + j - 1) = temp;
+            };
+        };
+    };
+}
+
 int main()
 {
     int len = 10;
     int arr[10];
     int* arr_ptr = &arr[0];
+
     fillWithRand(arr_ptr, len);
-    
-    for(int i = 0; i < len; i++){
-        cout << arr[i] << endl;
-    };
+    displayArray(arr_ptr, len);
+    bubbleSort(arr_ptr, len);
+    displayArray(arr_ptr, len);
     
     return 0;
 }

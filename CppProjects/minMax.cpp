d2e3b988339e0f90e2c062ccbed454d69f33543a
Vicvip23 <wicekwiacek@gmail.com>
2024-03-01 12:41:59 +0100

    added min and max search program

--- CppProjects/minMax.cpp
+++ CppProjects/minMax.cpp
@@ -25,7 +25,6 @@ int* searchMin(int* arr, int len){
 
         if(min > *(arr + i)){
             min_ptr = (arr + i);
-            min = *(arr + i);
         };
 
     };
@@ -43,7 +42,6 @@ int* searchMax(int* arr, int len){
 
         if(max < *(arr + i)){
             max_ptr = (arr + i);
-            max = *(arr + i);
         };
 
     };
@@ -52,19 +50,11 @@ int* searchMax(int* arr, int len){
 
 }
 
-void displayArray(int* arr, int len){
-    for(int i = 0; i < len; i++){
-        cout << *(arr + i) << " ";
-    };
-    cout << endl;
-}
-
 int main(){
 
     int numbers[10];
     fillWithRand(&numbers[0], 10);
     cout << *searchMin(&numbers[0], 10) << endl;
     cout << *searchMax(&numbers[0], 10) << endl;
-    displayArray(&numbers[0], 10);
 
 }
